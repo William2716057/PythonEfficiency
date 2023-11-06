9f8cb3fd54dc002fbfea46ffee499ef989f9b705
William Spriggs <Willspri@gmail.com>
2023-11-07 12:56:58 +1300

    update

--- PythonEfficiency.py
+++ PythonEfficiency.py
@@ -2,13 +2,25 @@ import time
 
 def brewCoffee():
     print("Start brewCoffee()")
+    #will take three seconds to complete
     time.sleep(3)
     print("End brewCoffee()")
     return "Brewing Complete"
 
 def makeToast():
     print("Start toast()")
+    #two seconds to complete
     time.sleep(2)
     print("End toasting()")
     return "Toasting complete()"
 
+#record start times
+def main():
+    startTime = time.time()
+
+    resultCoffee = brewCoffee()
+    resultToast = makeToast()
+
+    endTime = time.time()
+
+    elapsedTime = endTime - startTime
