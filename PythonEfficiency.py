5cabfb8d914a52a0ddf1368ec675149a6093f2ce
William Spriggs <Willspri@gmail.com>
2023-11-07 12:53:27 +1300

    Add project files.

---
+++ PythonEfficiency.py
@@ -0,0 +1,14 @@
+import time
+
+def brewCoffee():
+    print("Start brewCoffee()")
+    time.sleep(3)
+    print("End brewCoffee()")
+    return "Brewing Complete"
+
+def makeToast():
+    print("Start toast()")
+    time.sleep(2)
+    print("End toasting()")
+    return "Toasting complete()"
+
